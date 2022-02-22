0a4228a23002b574e5d050ae6d65d5d2ce379bdd
Tran Tien Dat <dattrantienht@gmail.com>
2022-02-22 21:32:04 +0700

    .

--- fish/config.fish
+++ fish/config.fish
@@ -1,8 +1,18 @@
 if status is-interactive
   # Commands to run in interactive sessions can go here
-
-  neofetch
   
+  neofetch
+
   starship init fish | source
 
 end
+
+function fish_greeting
+  #echo The time is (set_color yellow; date +%T; set_color normal)
+end
+
+#alias
+
+alias v="nvim"
+alias m="ncspot"
+alias ll="ls -la"
